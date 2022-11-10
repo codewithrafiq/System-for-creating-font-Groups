545a88483463807397a32d34a7e922e53f74a7f1
codewithrafiq <codewithrafiq@gramil.com>
2022-11-11 00:46:49 +0600

    .ttf file font tested

--- src/Pages/FontUpload.jsx
+++ src/Pages/FontUpload.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const FontUpload = () => {
+  const convertFontFiletoBase64 = async (file) => {
+    const result_base64 = await new Promise((resolve) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result);
+      fileReader.readAsDataURL(file);
+    });
+    return result_base64;
+  };
+
+  const onFileInput = async (e) => {
+    const { files } = e.currentTarget;
+    if (files && files?.length > 0) {
+      const font = await convertFontFiletoBase64(files[0]);
+
+      // @ts-ignore
+      const fontFace = new FontFace("uploadedFont", `url(${font})`);
+      document.fonts.add(fontFace);
+      await fontFace.load();
+    }
+  };
   return (
     <div>
-      <h1>Font Upload</h1>
+      <div className="container">
+        <input type="file" onInput={onFileInput} accept={".ttf"} />
+        <span className="text" style={{ fontFamily: "uploadedFont" }}>
+          Sample Text
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FontUpload
+export default FontUpload;
